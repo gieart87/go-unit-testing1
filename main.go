0b95ec1894d72727e0b9f6cc7c0085ce442cff8c
sugiarto <sugiarto@gie-art.com>
2020-08-12 12:48:47 +0700

    Init commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func Add(value1 int, value2 int) int {
+	return value1 + value2
+}
+
+func Substract(value1 int, value2 int) int {
+	return value1 - value2
+}
+
+func RootEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(200)
+	response.Write([]byte("Hello World"))
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", RootEndpoint).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":12345", router))
+}
